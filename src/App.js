b32545546f28c95cfb4480037c094b08c8685dc8
DavitAnt <59206166+DavitAnt@users.noreply.github.com>
2019-12-24 21:58:43 +0400

    initial setup parcel

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Combobox from "./Combobox";
+const App = () => {
+  return (
+    <div>
+      <h1>Combobox demo</h1>
+      <Combobox />
+    </div>
+  );
+};
+
+export default App;
